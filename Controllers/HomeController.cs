c911772e75451fe3f8a5e81603afdebdc21690ee
stedankyi <stedankyi@gmail.com>
2023-01-19 05:59:30 +0000

    Updated project files with files from personal computer

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
-using System.Diagnostics;
+namespace PartyInvites.Controllers;
 
-namespace PartyInvites.Controllers
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    public IActionResult Index()
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        return View();
     }
+
+    [HttpGet]
+    public ViewResult RsvpForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse)
+    {
+        Repository.AddResponse(guestResponse);
+        return View("Thanks", guestResponse);
+    }
+
+    public ViewResult ListResponses()
+    {
+        return View(Repository.Responses.Where(r => r.WillAttend == true));
+    }
+
 }
--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -1,9 +1,8 @@
-namespace PartyInvites.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
+namespace PartyInvites.Models;
 
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
